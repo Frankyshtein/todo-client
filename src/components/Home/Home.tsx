e7acf7fba8df14b297e473509c8ac8bebf2fbf07
takoi <takoiwebile@gmail.com>
2021-09-16 22:54:58 +0200

    added test request

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,10 @@
 export const Home = () => {
-  return <h1>Home Fucking Page</h1>
-}
+  return (
+    <>
+      <h1>Home Fucking Page</h1>
+      <form action="http://localhost:4000/test/" method="GET">
+        <button>Send request</button>
+      </form>
+    </>
+  );
+};

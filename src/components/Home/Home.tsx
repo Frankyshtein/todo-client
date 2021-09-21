cbe01ab210f74321f15d89a963c38b6230301f10
takoi <takoiwebile@gmail.com>
2021-09-21 20:00:32 +0200

    added test post request

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,22 @@
 export const Home = () => {
   const getData = () => {
-    fetch("http://localhost:4000/test/").then((response) => {
-        response.json().then(text => alert(text.text));
-      }
-    );
+    const body = JSON.stringify({ title: 'test' });
+    console.log(body);
+
+    fetch('http://localhost:4000/add-todo', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer',
+      body,
+    }).then((response) => {
+      // response.json().then((text) => alert(text.text));
+    });
   };
 
   return (
